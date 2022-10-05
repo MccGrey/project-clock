3fe95e6ab80f8ee16c2c3314f6dda26b740e0102
MccGrey <chuxzzie01@yahoo.com>
2022-10-05 04:40:53 +0100

    update

--- main.js
+++ main.js
@@ -32,6 +32,9 @@ function displayTime() {
     greet = "🌙 good evening";
     document.body.style.backgroundImage = "url(./assets/buena_noches.jpg)";
   }
+  if (hrs < 10) {
+    hrs = "0" + hrs;
+  }
 
   if (min < 10) {
     min = "0" + min;
